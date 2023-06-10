c3f87a1d6a52b944551d7647fe987b48a869dd61
Prabhu <prasadprabhu2222@gmail.com>
2023-06-10 20:06:03 +0530

    Encapsulation assignment First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly  obj=new RWOnly();
+//        obj.name="Akash";
+        //cannot change name here as it is a private member of the class RWOnly
+        obj.setName("Govind");
+        obj.getName();
+    }
   
 }
